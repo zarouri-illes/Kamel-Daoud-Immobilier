9442c9535a1a86f3085471489a12947e1ec17679
illes <illeszarouri@gmail.com>
2023-12-25 01:58:04 +0100

    file structure

---
+++ app/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css";
+
+const Navbar = () => (
+    <div className="navBar">
+        <div className="logoDiv">
+            <img src="/logo.jpg" alt="" />
+        </div>
+    </div>
+)
+
+export default Navbar
