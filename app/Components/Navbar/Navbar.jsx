656d16cd81402827b50f8e735e84b5a405b0c825
illes <illeszarouri@gmail.com>
2023-12-27 23:27:12 +0100

    added the navbar

--- app/Components/Navbar/Navbar.jsx
+++ app/Components/Navbar/Navbar.jsx
@@ -1,11 +1,25 @@
 import "./Navbar.css";
+import { navbar } from "../const";
+import Link from "next/link";
+import { HiBars3BottomRight } from "react-icons/hi2";
 
 const Navbar = () => (
     <div className="navBar">
         <div className="logoDiv">
-            <img src="/logo.jpg" alt="" />
+            <img src="/logo.png" alt="Kamel Daoud Immobilier" />
         </div>
+
+        <div className="menu">
+            <ul>
+                {navbar.map((item) => (
+                    <li><Link href={item.link} className="navList">{item.title}</Link></li>
+                ))}
+            </ul>
+        </div>
+        
+        <HiBars3BottomRight className="icon"/>
+
     </div>
 )
 
-export default Navbar
+export default Navbar;
--- app/Components/const.js
+++ app/Components/const.js
@@ -1,5 +1,25 @@
 import Navbar from "./Navbar/Navbar";
 
+const navbar = [
+    {
+        title: "Acceuil",
+        link: "#acceuil",
+    },
+    {
+        title: "À propos",
+        link: "#àpropos",
+    },
+    {
+        title: "Places",
+        link: "",
+    },
+    {
+        title: "",
+        link: "",
+    }
+]
+
 export {
-    Navbar
+    Navbar,
+    navbar
 }
