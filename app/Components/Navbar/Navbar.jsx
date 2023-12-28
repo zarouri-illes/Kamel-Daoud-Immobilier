53551cde2bab6026c6b80ec1894665fa790e877a
illes <illeszarouri@gmail.com>
2023-12-28 16:28:47 +0100

    finished the navbar

--- app/Components/Navbar/Navbar.jsx
+++ app/Components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
+"use client"
+
 import "./Navbar.css";
+import React, {useState} from "react";
 import { navbar } from "../const";
 import Link from "next/link";
 import { HiBars3BottomRight } from "react-icons/hi2";
+import { IoMdCloseCircle } from "react-icons/io";
 
-const Navbar = () => (
+const Navbar = () => {
+
+    const [navBar, setNavBar] = useState('menu');
+
+    return (
     <div className="navBar">
         <div className="logoDiv">
             <img src="/logo.png" alt="Kamel Daoud Immobilier" />
@@ -15,11 +23,15 @@ const Navbar = () => (
                     <li><Link href={item.link} className="navList">{item.title}</Link></li>
                 ))}
             </ul>
+
+            <IoMdCloseCircle className="closeIcon icon"/>
+
         </div>
         
-        <HiBars3BottomRight className="icon"/>
+        <HiBars3BottomRight className="icon menuIcon"/>
 
     </div>
 )
+}
 
 export default Navbar;
